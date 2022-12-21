d10fd0b073cc80ea6d4ed1cd451333fe430a2689
Daniel Abdoral <115808976+Abdoral97@users.noreply.github.com>
2022-12-21 11:12:36 -0300

    finalizando projeto

--- assets/script.js
+++ assets/script.js
@@ -2,24 +2,24 @@ const seconds = document.getElementById("segundos")
 const minutes = document.getElementById("minutos")
 const hours = document.getElementById("horas")
 
-const relogio = setInterval(function(){
-    
+const relogio = setInterval(function () {
+
     const data = new Date();
     let s = data.getSeconds();
     let min = data.getMinutes();
     let hr = data.getHours();
 
-    if (s < 10) {s = "0" + s}
-    
-    if (min < 10){min = "0" + min}
+    if (s < 10) { s = "0" + s }
 
-    if (hr < 10) {hr = "0" + hr}
- 
-    seconds.innerText=(s)
-    minutes.innerText=(min)
-    hours.innerText=(hr)
+    if (min < 10) { min = "0" + min }
 
-    
+    if (hr < 10) { hr = "0" + hr }
+
+    seconds.innerText = (s)
+    minutes.innerText = (min)
+    hours.innerText = (hr)
+
+
 
 }, 1000)
 
